9667acb0c4d51a04dade40b689737fc204668ab9
adrian-jordan <adrianjordan@hotmail.co.uk>
2019-12-03 09:57:00 +0000

    added constants file

---
+++ constants.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+This is a file to store all base values for the game.
+"""
+import os.path
+
+DIR = os.path.dirname(__file__)
+IMG_DIR = os.path.join(DIR, 'images')
+
+
+SCREEN_X = 800
+SCREEN_Y = 600
+
+HP = 1000
+ATT = 10
+DEF = 10
+GOLD = 0
+EXP = 0
+RK = 1
+BK = 1
+YK = 1
