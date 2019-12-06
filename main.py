3426def8a580fc3bc1c195a1e92717a7848e1d9a
adrian-jordan <adrianjordan@hotmail.co.uk>
2019-12-06 20:05:06 +0000

    various fixes

--- main.py
+++ main.py
@@ -44,38 +44,30 @@ def init_overlay(level, struct, floorNum):
             if obj[0] == 'm':
                 struct[key] = get_monster(int(obj[1:]))
                 struct[key].set_position(SCREEN_X / 13 + SCREEN_X / 13 * column, SCREEN_Y / 13 + SCREEN_Y / 13 * row)
-                struct[key].add_to_group()
             elif obj[0] == 's':
                 if int(obj[-1]) > floorNum:
                     struct[key] = StairUp('Map', SCREEN_X / 13, SCREEN_Y / 13)
                 else:
                     struct[key] = StairDown('Map', SCREEN_X / 13, SCREEN_Y / 13)
                 struct[key].set_position(SCREEN_X / 13 + SCREEN_X / 13 * column, SCREEN_Y / 13 + SCREEN_Y / 13 * row)
-                struct[key].add_to_group()
             elif obj == 'YD':
                 struct[key] = YellowDoor('Map', SCREEN_X / 13, SCREEN_Y / 13)
                 struct[key].set_position(SCREEN_X / 13 + SCREEN_X / 13 * column, SCREEN_Y / 13 + SCREEN_Y / 13 * row)
-                struct[key].add_to_group()
             elif obj == 'BD':
                 struct[key] = BlueDoor('Map', SCREEN_X / 13, SCREEN_Y / 13)
                 struct[key].set_position(SCREEN_X / 13 + SCREEN_X / 13 * column, SCREEN_Y / 13 + SCREEN_Y / 13 * row)
-                struct[key].add_to_group()
             elif obj == 'RD':
                 struct[key] = RedDoor('Map', SCREEN_X / 13, SCREEN_Y / 13)
                 struct[key].set_position(SCREEN_X / 13 + SCREEN_X / 13 * column, SCREEN_Y / 13 + SCREEN_Y / 13 * row)
-                struct[key].add_to_group()
             elif obj[1] == 'K':
                 struct[key] = Key(KEYS[obj], SCREEN_X/13, SCREEN_Y/13,obj)
                 struct[key].set_position(SCREEN_X/13+SCREEN_X/13*column, SCREEN_Y/13+SCREEN_Y/13*row)
-                struct[key].add_to_group()
             elif obj[0] == 'i' :
                 struct[key] = OtherItem(ITEMS[obj], SCREEN_X/13, SCREEN_Y/13,obj)
                 struct[key].set_position(SCREEN_X/13+SCREEN_X/13*column, SCREEN_Y/13+SCREEN_Y/13*row)
-                struct[key].add_to_group()
             elif obj[0] == 'f':
                 struct[key] = NPC(NPCS[obj], SCREEN_X/13, SCREEN_Y/13,obj)
                 struct[key].set_position(SCREEN_X/13+SCREEN_X/13*column, SCREEN_Y/13+SCREEN_Y/13*row)
-                struct[key].add_to_group()
 
         column += 1
 
@@ -120,12 +112,18 @@ def draw_outside(struct, surf):
 
 def add_all_to_group(objects):
     for key in objects:
-        objects[key].add_to_group()
+        try:
+            objects[key].add_to_group()
+        except:
+            pass
 
 
 def remove_all_from_group(objects):
     for key in objects:
-        objects[key].kill()
+        try:
+            objects[key].add_to_group()
+        except:
+            pass
 
 
 def draw_stats(stats, surf):
@@ -135,32 +133,32 @@ def draw_stats(stats, surf):
 def fetch_floors():
     floors = {}
     for key in FLOORS:
-        print(key)
         floors[key] = init_floor(FLOORS[key], block_objects.copy())
     return floors
 
-def fetch_overlays():
+def fetch_overlays(player):
     overlays = {}
     for key in floor_overlays:
         if key > 1:
             break
-        overlays[key] = init_floor(FLOORS[key], block_objects.copy())
+        overlays[key] = init_overlay(floor_overlays[key], block_objects.copy(), player.FLOOR)
         
     return overlays
 
+player = Player('Player', SCREEN_X / 13, SCREEN_Y / 13)
 world_floors = fetch_floors()
-world_overlays = fetch_overlays()
+world_overlays = fetch_overlays(player)
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
-player = Player('Player', SCREEN_X / 13, SCREEN_Y / 13)
-overlay = init_overlay(floor_overlays[player.FLOOR - 1], overlay_objects.copy(), player.FLOOR - 1)
+#overlay = init_overlay(floor_overlays[player.FLOOR - 1], overlay_objects.copy(), player.FLOOR - 1)
 init_player(player, floor_overlays[player.FLOOR - 1], 1)
 add_all_to_group(world_floors[player.FLOOR - 1])
+add_all_to_group(world_overlays[player.FLOOR - 1])
 running = True
 
 while running:
     draw_floor(world_floors[player.FLOOR - 1], screen)
-    draw_overlay(overlay, screen)
+    draw_overlay(world_overlays[player.FLOOR - 1], screen)
     draw_outside(OUTSIDE, screen)
     player.draw(screen)
     pygame.display.flip()
@@ -172,9 +170,10 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
             else:
-                player.update(event.key, overlay, floor_overlays[player.FLOOR - 1])
+                player.update(event.key, world_overlays[player.FLOOR - 1], floor_overlays[player.FLOOR - 1])
     if player.FLOOR - 1 != temp:
         remove_all_from_group(world_floors[temp])
+        remove_all_from_group(world_overlays[temp])
         COLLISION_TYPE.empty()
         overlay = init_overlay(floor_overlays[player.FLOOR - 1], overlay_objects.copy(), player.FLOOR - 1)
         if player.FLOOR - 1 > temp:
@@ -182,6 +181,8 @@ while running:
         else:
             init_player(player, floor_overlays[player.FLOOR - 1], 2)
         add_all_to_group(world_floors[player.FLOOR - 1])
+        add_all_to_group(world_overlays[player.FLOOR - 1])
 
 remove_all_from_group(world_floors[player.FLOOR - 1])
+remove_all_from_group(world_overlays[temp])
 pygame.quit()
