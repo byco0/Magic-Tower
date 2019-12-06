202684f06de5565c35bacb19bf2ad5ea2ccfd255
adrian-jordan <adrianjordan@hotmail.co.uk>
2019-12-06 14:51:46 +0000

    minor changes

--- stats_text_handler.py
+++ stats_text_handler.py
@@ -3,14 +3,14 @@ import pygame
 pygame.init()
 pygame.font.init()
 
-def draw_stats(player, x, y):
-    surf = pygame.Surface((x, y))
+def draw_stats(player, width, height):
+    surf = pygame.Surface((width, height))
     font = pygame.font.Font(None, 26)
-    new_x = x/4
-    new_y = y/10
+    new_x = width/4
+    new_y = height/14*2
     for key in player.stats:
         stat_text = font.render('{}: {}'.format(key, player.stats[key]), True, WHITE)
         surf.blit(stat_text, (x, y-stat_text.get_height()))
-        new_y += y/10
+        new_y += height/14
         
     return surf
